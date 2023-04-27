3e77fbc75d76b7b1a5e2e730ca93bcb62cdcce68
Diana <105229986+diana2907@users.noreply.github.com>
2023-04-27 17:19:14 +0300

    +

---
+++ src/components/AppTweets/AppTweets.jsx
@@ -0,0 +1,11 @@
+import css from 'components/AppTweets/AppTweets.module.css';
+
+export const AppTweets = () => {
+  <>
+    <ul>
+      <li>
+        <div className={css.wrap}></div>
+      </li>
+    </ul>
+  </>;
+};
